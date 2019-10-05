3be460ca3caa5cabe7e07dae33bf205f687518ea
Mikhail <gagin@mysmartflat.ru>
2019-10-05 21:37:00 +0500

    Initial Commit

---
+++ LittleHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LittleHelper
+//
+//  Created by Михаил on 05/10/2019.
+//  Copyright © 2019 M57. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
